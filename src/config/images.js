abbdb9c1526bd52600ff8a88b2887ca1e8461c06
Shimaa Hussein <shussien@curpha.com>
2022-04-10 21:46:08 +0200

    add splash screen + logo

---
+++ src/config/images.js
@@ -0,0 +1,12 @@
+/* App config for images
+ */
+
+const images = {
+    splashScreen: require("../assets/img/splashScreen.png"),
+    shortVideosLogo:require("../assets/img/shortVideosLogo.png"),
+    phone:require("../assets/img/phone.jpeg"),
+    logo:require("../assets/img/logo.png"),
+
+};
+
+export default images;
